072dca4fe17d0d5136e13740a82fcab28a6138dc
Meck <yesmeck@gmail.com>
2014-03-15 07:36:10 +0800

    Add Ember

--- lib/lanaya/web.rb
+++ lib/lanaya/web.rb
@@ -11,13 +11,28 @@ module Lanaya
   class Web < Sinatra::Base
     register Sinatra::Reloader
 
-    dir = File.dirname(File.expand_path(__FILE__))
+    web_dir = File.expand_path('../../../web', __FILE__)
 
-    set :root,  "#{dir}/web"
+    set :public_folder, File.join([web_dir, 'tmp', 'result'])
+
+    get '/config/*' do |path|
+      send_file File.join([web_dir, 'config', path])
+    end
+
+    get '/vendor/*' do |path|
+      send_file File.join([web_dir, 'vendor', path])
+    end
+
+    get '/tests' do |path|
+      send_file File.join([settings.public_folder, 'tests', 'index.html'])
+    end
+
+    get '/tests/*' do |path|
+      send_file File.join([web_dir, 'tests', path])
+    end
 
     get '/' do
-      @records = []
-      erb :index
+      send_file File.join([settings.public_folder, 'index.html'])
     end
 
     get '/:session_id/response_body' do |session_id|
